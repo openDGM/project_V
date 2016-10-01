fafc19083bd4a87da549b9fd2f973e1e3a270bb1
Andreas.Tack <a.tack@gmx.net>
2016-10-01 22:32:28 +0300

    Comments

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 IDIR = include
-EIGDIR = ../Eigen
+EIGDIR = ../eigen
 CXXFLAGS = -std=c++11
 
 all : build/bin/openDGM
 
-build/bin/openDGM : build/obj/functions1D.o build/obj/edge.o build/obj/element.o
+build/bin/openDGM : build/obj/functions2D.o build/obj/functions1D.o build/obj/edge.o build/obj/element.o
 	mkdir -p build/bin
 	g++ $(CXXFLAGS) -I$(IDIR) -I$(EIGDIR) build/obj/functions1D.o build/obj/edge.o build/obj/element.o main.cpp -o $@
 
@@ -19,6 +19,10 @@ build/obj/element.o : source/element.cpp
 build/obj/functions1D.o : source/functions1D.cpp
 	mkdir -p build/obj
 	g++ -c $(CXXFLAGS) -I$(IDIR) -I$(EIGDIR) source/functions1D.cpp -o $@
+
+build/obj/functions2D.o : source/functions2D.cpp
+	mkdir -p build/obj
+	g++ -c $(CXXFLAGS) -I$(IDIR) -I$(EIGDIR) source/functions2D.cpp -o $@
 
 clean : 
 	rm build/obj/*.o build/bin/*
--- include/functions2D.h
+++ include/functions2D.h
@@ -7,11 +7,13 @@
 
 /*
  * Description:
- * A collection of functions needed to create the Nth order basis in for two dimensional nodal elements in the DGM discretization.
+ * A collection of functions needed to create the Nth order basis for two dimensional nodal elements in the DGM discretization.
  *
- * N : refers to the order of the polynomial base
- * r : refers to location of a single node in the reference element on interval -1 <= r <= 1
- * x : refers to location of a single node in the generic element on interval XLeft <= x <= XRight
+ * N : refers to the order of the polynomial in first dimension
+ * M : refers to the order of the polynomial in second dimension
+ * r : refers to location of node on 1st dimension in the reference element on interval -1 <= r <= 1,
+ * s : refers to location of node on 2nd dimension in the reference element on interval -1 <= s <= 1
+ 
  */
 
 #include <Eigen/Dense>
@@ -29,10 +31,10 @@ namespace functions2D{
 ArrayXd jacobiP2D(const ArrayXd& r, const ArrayXd& s, int alpha, int beta, int N, int M);
 
 // return gradient in r-direction of 2D jacobi polynomial of Nth order evaluated at locations r,s
-VectorXd GradRJacobiP2D(const VectorXd& r, const ArrayXd & s, int alpha, int beta, int N, int M);
+VectorXd GradRJacobiP2D(const VectorXd& r, const ArrayXd& s, int alpha, int beta, int N, int M);
 
 // return gradient in s-direction of 2D jacobi polynomial of Nth order evaluated at locations r,s
-VectorXd GradSJacobiP2D(const VectorXd& r, const ArrayXd & s, int alpha, int beta, int N, int M);
+VectorXd GradSJacobiP2D(const VectorXd& r, const ArrayXd& s, int alpha, int beta, int N, int M);
 
 // return the Vandermonde matrix for the Nth order reference element 
 MatrixXd Vandermonde2D(int N, const VectorXd& r, const VectorXd& s);
@@ -48,5 +50,6 @@ SparseMatrix<double> DRMatrix2D(int N, const VectorXd& r, const VectorXd& s);
 
 // return Differentiation matrix/operator for the Nth order reference element
 SparseMatrix<double> DSMatrix2D(int N, const VectorXd& r, const VectorXd& s);
+
 }
 #endif
